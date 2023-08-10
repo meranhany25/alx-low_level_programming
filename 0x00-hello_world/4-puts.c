dc2242bd114b084ba1ab34f09b6548d677704e8d
meranhany25 <meranhany25@gmail.com>
2023-08-10 16:17:18 -0700

    4-puts.c00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main  - A program that prints a line with puts function
+* main - Entry point
 *
-* Return : 0 (Success)
-*/
+*
+* Description: A C program that prints with put function
+*
+*Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"programming is like building  a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
