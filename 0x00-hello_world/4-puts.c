f40e4b01e6af13af65720445b7189a9576db945f
meranhany25 <meranhany25@gmail.com>
2023-08-10 15:28:00 -0700

    099

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main  - A program that prints a line with puts function
- * Return : 0 (Success)
- */
+* main  - A program that prints a line with puts function
+*
+* Return : 0 (Success)
+*/
 int main(void)
 {
 puts("\"programming is like building  a multilingual puzzle");
