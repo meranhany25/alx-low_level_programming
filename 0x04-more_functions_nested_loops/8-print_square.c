66c1bd90dc9466d7928534d0c0933131647735d9
meranhany25 <meranhany25@gmail.com>
2023-08-17 12:41:40 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,17 +3,26 @@
 /**
  * print_square - print a square using the character #
  * @size: is the size of the square
- * Return: Always 0 (Success
+ * Return: Always 0 (Success)
  */
 
 void print_square(int size)
 {
-	int row, column;
-
-	for (row = 1; row <= size; row++)
+	if (size <= 0)
 	{
-		for (column = 1; column <= size; column++)
-			_putchar('#');
 		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+
+		}
 	}
 }
