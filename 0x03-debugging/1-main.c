1aabaad4c057c7e14b3521216d1ae82ceb87e9f1
meranhany25 <meranhany25@gmail.com>
2023-08-17 16:23:28 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,6 +12,7 @@ int main(void)
 	printf("Infinte loop incoming :(\n");
 
 	i = 0;
+
 	/**
 	*while (i < 10)
 	*{
@@ -19,6 +20,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
