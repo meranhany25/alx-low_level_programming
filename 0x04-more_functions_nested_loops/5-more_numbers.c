d1ad1d06e72baa71a308f859c72b8916c4cf0fc0
meranhany25 <meranhany25@gmail.com>
2023-08-17 11:02:05 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 	{
 		for (count = 0; count <= 14; count++)
 		{
-			count = num;
+			num = count;
 			if (count > 9)
 			{
 				_putchar(1 + 48);
-				 num = count % 10;
+				num = count % 10;
 			}
 			_putchar(num + 48);
 		}
