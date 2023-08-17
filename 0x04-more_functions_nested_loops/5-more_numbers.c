7e872e08c9a43ff2db5c973dc6dfc771465340d9
meranhany25 <meranhany25@gmail.com>
2023-08-17 08:56:21 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * more_numbers - print 0 - 14 ten times and you
  * can only use _putchar three times
  * Return: Always 0 (Success)
  */
-
 void more_numbers(void)
 {
 	int num, row, count;
