c039b4fc64f6a8e03a5cb4356032370cd6b2c36b
meranhany25 <meranhany25@gmail.com>
2023-08-24 08:17:55 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
 
-	while (s1)
+	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
-			equal = ((int) *s1 - 48) - ((int) *s2 - 48);
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
 		s1++;
