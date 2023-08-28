fff35a2c9f1d250748df3d4cbcc6e43a0eabca2c
meranhany25 <meranhany25@gmail.com>
2023-08-28 15:44:00 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 #endif /* main_h */
