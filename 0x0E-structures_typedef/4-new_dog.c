0145c1e86fdb9056b110bbd4ddbad9da58e79073
meranhany25 <meranhany25@gmail.com>
2023-09-11 15:20:24 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,9 @@ int _strlen(const char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i]; i++)
+
 		dest[i] = src[i];
 	dest[i] = '\0';
 	return (dest);
@@ -49,7 +51,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog ->name = malloc(sizeof(char)* (_strlen(name) + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
 		free(dog);
