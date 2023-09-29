72c337cc964c533a3a81f7e0d8c550acff00edd8
meranhany25 <meranhany25@gmail.com>
2023-09-30 01:11:14 +0300

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*flip_bits - return number of bits that would need to be flipped
+*@n: first number
+*@m: second number
+*Return: number of bits to flip to convert number
+**/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xorval = n ^ m;
+unsigned int count = 0;
+while (xorval)
+{
+if (xorval & 1ul)
+count++;
+xorval = xorval >> 1;
+}
+return (count);
+}
