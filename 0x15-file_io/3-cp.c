8d58c9e2cd446fa3ce77baa201af590e87fbb672
meranhany25 <meranhany25@gmail.com>
2023-10-03 01:44:08 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,41 @@
 #include "main.h"
-/**
-  * main - Write a program that copies the content of a file to another file
-  * @ac: argument count
-  * @av: array of argument tokens
-  * Return: Always 0. (Success)
-  */
-int main(int ac, char *av[])
-{
-	int taken_from, taken_to, started, begin;
-	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
-	char buffer[BUFSIZE];
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	taken_from = open(av[1], O_RDONLY);
-	if (taken_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	taken_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
-	if (taken_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	started = 1;
-	while (started)
-	{
-		started = read(taken_from, buffer, BUFSIZE);
-		if (started == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (started > 0)
-		{
-			begin = write(taken_to, buffer, started);
-			if (begin != started || begin == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-		}
-	}
-	if (close(taken_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", taken_from), exit(100);
-	if (close(taken_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", taken_to), exit(100);
-	return (0);
+#define USAGE "usage cp file_from file_to\n"
+#define ERR_NOREAD "Error: can't read from file %s\n"
+#define ERR_NOWRITE "Error: can't close fd %d\n"
+#define ERR_NOCLOSE "Error: can't close fd %d\n"
+#define PRENISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * main - program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 1 on success 0 on failure
+*/
+int main(int ac, char **av)
+{
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
+	char buf[READ_BUF_SIZE];
+
+if (ac != 3)
+dprintf(STDERR_FILENO, USAGE), exit(97);
+from_fd = open(av[1], O_RDONLY);
+if (from_fd == -1)
+dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PRENISSIONS);
+if (to_fd == -1)
+dprintf(STDERR_FILENO, ERR_NOREAD, av[2]), exit(99);
+while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+if (write(to_fd, buf, b) != b)
+dprintf(STDERR_FILENO, ERR_NOREAD, av[2]), exit(99);
+if (b == -1)
+dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+from_fd = close(from_fd);
+to_fd = close(to_fd);
+if (from_fd)
+dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+if (to_fd)
+dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+return (EXIT_SUCCESS);
 }
