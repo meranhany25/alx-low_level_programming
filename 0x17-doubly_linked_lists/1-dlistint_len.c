1c888b36e4ee09c2fb7d3fd775919088f9d20551
meranhany25 <meranhany25@gmail.com>
2023-11-09 04:41:54 -0800

    dn

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,13 +10,12 @@
 size_t print_dlistint_len(const dlistint_t *h)
 {
         size_t count = 0;
-	int lenght;
 
         while (h != NULL)
         {
                 printf("%d\n", h->n);
                 count++;
                 h = h->next;
-		lenght = length(count);
         }
-        return (lenght);
+        return (count);
+}
